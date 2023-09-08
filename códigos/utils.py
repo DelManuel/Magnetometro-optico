8ea759229169609c7e2b8e1917d87a78efd3fa0d
nicofis <nicofische@gmail.com>
2023-09-08 18:17:20 -0300

    rompimos la fuente...otra vez :/

--- códigos/utils.py
+++ códigos/utils.py
@@ -6,7 +6,7 @@ import numpy as np
 from tqdm import tqdm
 from typing import Tuple
 
-def medir(duracion: float, fs: int, device_name: str, bounds = [-10,10]) -> np.ndarray:
+def medir(duracion: float, fs: int, device_name: str, bounds = [-10,10], modo = "DIFF", ch2 = True, bounds2 = [-10,10], modo2 = "DIFF") -> np.ndarray:
     """
     Medir un canal de la DAQ.
 
@@ -20,7 +20,12 @@ def medir(duracion: float, fs: int, device_name: str, bounds = [-10,10]) -> np.n
     cant_puntos = int(duracion*fs) # Tiempo a medir por cantidad de puntos por segundo
 
     # Configuraciones del canal de medicion
-    terminal_config = nidaqmx.constants.TerminalConfiguration.RSE
+    if modo == "DIFF": # Para medir en modo diferencial 
+        terminal_config = nidaqmx.constants.TerminalConfiguration.DIFF
+    
+    else: # Para medir en modo Referenced Single-Ended
+        terminal_config = nidaqmx.constants.TerminalConfiguration.RSE
+
     sample_mode = nidaqmx.constants.AcquisitionType.FINITE
     samples_per_channel = nidaqmx.constants.READ_ALL_AVAILABLE
 
@@ -31,6 +36,16 @@ def medir(duracion: float, fs: int, device_name: str, bounds = [-10,10]) -> np.n
                                              terminal_config = terminal_config, 
                                              max_val=bounds[1], 
                                              min_val=bounds[0])
+        
+        if ch2:
+            if modo2 == "DIFF": # Para medir en modo diferencial 
+                terminal_config_2 = nidaqmx.constants.TerminalConfiguration.DIFF
+            else:
+                terminal_config_2 = nidaqmx.constants.TerminalConfiguration.RSE
+            task.ai_channels.add_ai_voltage_chan(f"{device_name}/ai3", 
+                                             terminal_config = terminal_config_2, 
+                                             max_val=bounds2[1], 
+                                             min_val=bounds2[0])
         
         # Medir una cantidad de puntos.
         task.timing.cfg_samp_clk_timing(fs, 
@@ -43,7 +58,7 @@ def medir(duracion: float, fs: int, device_name: str, bounds = [-10,10]) -> np.n
 
 def medir_continuo(duracion: float = 5, 
                    fs: int = 300000, 
-                   max_iter: int = 10) -> Tuple[np.ndarray]:
+                   max_iter: int = 10, modo = "DIFF") -> Tuple[np.ndarray]:
 
     device_name = nidaqmx.system.System.local().devices[0].name
 
@@ -53,7 +68,7 @@ def medir_continuo(duracion: float = 5,
 
             t_0 = time.perf_counter()
 
-            y = medir(duracion, fs, device_name)
+            y = medir(duracion, fs, device_name, modo = modo)
             
             datos_y = np.concatenate([datos_y, y])
 
